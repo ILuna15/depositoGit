adb86f947dd927ebb628677e2c0ae2984ef50823
I <I@DESKTOP-UHBT0R8>
2020-01-04 16:47:22 +0100

    Commint

--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -26,6 +26,7 @@ public class Main {
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
+        float cantidad;
         return cuenta1;
 	}   
 }
